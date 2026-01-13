5b82c13b58fed1a52ad3f635f6025fa4e6e486ff
speedfox3 <130868601+speedfox3@users.noreply.github.com>
2026-01-13 15:14:05 -0300

    new auth

--- docs/js/auth.js
+++ docs/js/auth.js
@@ -2,16 +2,21 @@
 const SUPABASE_URL = "https://fsudquiewxlktggtkyia.supabase.co";
 const SUPABASE_KEY = "sb_publishable_lp_Kgg0F012kzLDqHfcQvg_vg-pt-uV";
 
-const supabase = supabaseJs.createClient(
+const supabase = window.supabase.createClient(
   SUPABASE_URL,
-  SUPABASE_KEY
+  SUPABASE_ANON_KEY
 );
 
-function loginWithGoogle() {
-  supabase.auth.signInWithOAuth({
+async function loginWithGoogle() {
+  const { error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: window.location.origin + "/Space-Explorer/index.html"
+      redirectTo: "https://speedfox3.github.io/Space-Explorer/index.html"
     }
   });
+
+  if (error) {
+    console.error("Error login:", error.message);
+    alert("Error al iniciar sesión");
+  }
 }
