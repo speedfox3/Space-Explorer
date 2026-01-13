2e1b4e27ece01f4438c761627aa2e8e765ddb9fd
speedfox3 <130868601+speedfox3@users.noreply.github.com>
2026-01-13 12:36:59 -0300

    Change ship data API URL to production
    Updated the API URL for fetching ship data.

--- Space-MMO/Frontend/js/game.js
+++ Space-MMO/Frontend/js/game.js
@@ -1,6 +1,7 @@
 async function loadShip() {
   const res = await fetch("http://localhost:8000/api/ship/");
   const data = await res.json();
+  const API_URL = https://space-explorer-v2zx.onrender.com;
 
   document.getElementById("ship-info").innerHTML = `
     <p>Nave nivel: ${data.level}</p>
@@ -8,3 +9,4 @@ async function loadShip() {
     <p>Motor: ${data.engine}</p>
   `;
 }
+
