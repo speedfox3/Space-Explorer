c35fdf876ffc24958e48f06c07161127c7f86eb9
speedfox3 <130868601+speedfox3@users.noreply.github.com>
2026-02-09 13:38:02 -0300

    Delete docs directory

--- docs/js/supabase.js
+++
@@ -1,5 +0,0 @@
-// docs/js/supabase.js
-const SUPABASE_URL = "https://fsudquiewxlktggtkyia.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_lp_Kgg0F012kzLDqHfcQvg_vg-pt-uV";
-
-export const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
